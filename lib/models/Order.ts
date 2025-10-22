e193b6a9c43d91548c4bd3e29a6372cf7921bfcb
Khatanbaatar A <25lp5598@nest.edu.mn>
2025-10-22 21:00:20 +0800

    order schema

---
+++ lib/models/Order.ts
@@ -0,0 +1,41 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { FoodOrderStatusEnum } from "../enums/foodOrderStatusEnum";
+
+interface IFoodOrderItem {
+  food: mongoose.Types.ObjectId;
+  quantity: number;
+}
+
+export interface IFoodOrder extends Document {
+  user: mongoose.Types.ObjectId;
+  foodOrderItems: IFoodOrderItem[];
+  totalPrice: number;
+  status: FoodOrderStatusEnum;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const FoodOrderItemSchema = new Schema<IFoodOrderItem>({
+  food: { type: Schema.Types.ObjectId, ref: "Food", required: true },
+  quantity: { type: Number, required: true, min: 1 },
+});
+
+const FoodOrderSchema = new Schema<IFoodOrder>(
+  {
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    foodOrderItems: [FoodOrderItemSchema],
+    totalPrice: { type: Number, required: true },
+    status: {
+      type: String,
+      enum: Object.values(FoodOrderStatusEnum),
+      default: FoodOrderStatusEnum.PENDING,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const FoodOrder =
+  mongoose.models.FoodOrder ||
+  mongoose.model<IFoodOrder>("FoodOrder", FoodOrderSchema);
